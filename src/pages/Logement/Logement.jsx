6ca4df93a8a0e9d681873ab942e52eda4577ef23
boukhatem nesrine <meddah.nk@gmail.com>
2024-07-07 11:06:16 +0200

    mettre a jours

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,43 +1,41 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import data from "../../data/logements.json";
 import "./Logement.css";
 import Carrousel from "../../components/Carrousel/Carrousel";
 import Collapse from "../../components/Collapse/Collapse";
 import Tag from "../../components/Tag/Tag";
 import Stars from "../../components/Stars/Stars";
-
+//parametre
 export default function Logement() {
   const [logement, setLogement] = useState(null);
   const { id } = useParams();
-
+  const navigate = useNavigate();
+//chemin
   useEffect(() => {
     const getLogement = (data) => {
       const item = data.find((appart) => appart.id === id);
+      if (!item) {
+        return navigate("/Error");
+      }
       setLogement(item);
-    };
+    }; 
+
     getLogement(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   console.log(logement);
 
   if (logement === null) return <div>404</div>;
 
-  const testClick = () => {
-    console.log("v");
-  };
-
   return (
     <section>
       <Carrousel pictures={logement.pictures} />
-      {/* <img src= {logement.pictures.url}/>   
-            <div className="apprt_img"> 
-              
-                <img src={logement.cover}/>
-            </div> */}
+    
       <div className="apprt_headr">
         <div className="apprt_title">
-          <h1 onClick={testClick}> {logement.title}</h1>
+          <h1> {logement.title}</h1>
           <h2 className="apprt_soutitle"> {logement.location}</h2>
           <div className="tags-container">
             {logement.tags.map((tag) => (
